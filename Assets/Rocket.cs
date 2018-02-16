7fc3057e5f7452c3856f41bb81c3197294f34641
Ruan Xun <galaxymaxx@outlook.com>
2018-02-16 16:34:37 -0500

    Add a thurst sound for the rocket

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,13 @@ using UnityEditor;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-	Rigidbody rigidBody; 
+	Rigidbody rigidBody;
+
+	private AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,12 +21,17 @@ public class Rocket : MonoBehaviour {
 	private void ProcessInput() {
 		if (Input.GetKey(KeyCode.Space)) {
 			rigidBody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying) {	// so it doesn't layer
+				audioSource.Play();
+			}
+		} else {
+			audioSource.Stop();
 		}
 		if (Input.GetKey(KeyCode.A)) {
 			transform.Rotate(Time.deltaTime*Vector3.forward);
 		}
 		else if (Input.GetKey(KeyCode.D)) {
-			transform.Rotate(Time.deltaTime*(-Vector3.forward));
+			transform.Rotate(Time.deltaTime*-Vector3.forward);
 		}
 	}
 }
